a114abf045ac9b9a72ab9bb95693e265f95a0446
Seungwan Han <hseungwan8@gmail.com>
2023-05-01 20:41:50 +0900

    [0510 스터디 숙제] 숙제 문제 수정

--- src/main/java/com/example/practice/Controller/ProductController.java
+++ src/main/java/com/example/practice/Controller/ProductController.java
@@ -24,6 +24,7 @@ public class ProductController {
     2. 상품 조회 메서드 작성
     조건 : db내에 존재하는 모든 상품 리스트를 리턴할 것
     + 숙제 : 해당 엔티티를 엔티티의 리스트가 아니라 responseDTO를 작성해 List<responesDTO> 로 리턴할 것
+    + 정보 다 들어있고, 몇개의 리스트가 존재하는지 카운트도 리턴할 것
      */
 
 
@@ -32,5 +33,7 @@ public class ProductController {
     조건 : db내에 존재할경우 객체 전송 아닐경우 상품이없다고 전송
     + 숙제 : 마찬가지로 responseDTO로 캐스팅해서 리턴할 것
      */
+
+    // 1 < 3 << 2
 
 }
